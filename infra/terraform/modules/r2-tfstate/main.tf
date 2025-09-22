3194053e4e8cfd9f523d38abb0464b5ae34c3331
tqer39 <tqer39@gmail.com>
2025-09-22 09:39:29 +0900

    feat: remove Cloudflare R2 state module and associated variables

--- infra/terraform/modules/r2-tfstate/main.tf
+++
@@ -1,4 +0,0 @@
-resource "cloudflare_r2_bucket" "state" {
-  account_id = var.cloudflare_account_id
-  name       = var.r2_bucket_name
-}
--- infra/terraform/modules/r2-tfstate/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "cloudflare_account_id" {
-  description = "Cloudflare Account ID"
-  type        = string
-}
-
-variable "r2_bucket_name" {
-  description = "tfstate を保存する R2 バケット名"
-  type        = string
-}

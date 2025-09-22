8878b4396ce39685fcd4fe9b3689f810d6c156b5
tqer39 <tqer39@gmail.com>
2025-09-23 00:46:50 +0900

    feat: simplify IAM policy definitions and update references in deployment role

--- infra/terraform/modules/deploy_role/main.tf
+++ infra/terraform/modules/deploy_role/main.tf
@@ -19,74 +19,12 @@ data "aws_iam_policy_document" "assume_role" {
   }
 }
 
-data "aws_iam_policy_document" "deploy_allow_specifics" {
-  statement {
-    sid = "AllowSpecifics"
-    actions = [
-      "lambda:*",
-      "apigateway:*",
-      "ec2:*",
-      "rds:*",
-      "s3:*",
-      "sns:*",
-      "states:*",
-      "ssm:*",
-      "sqs:*",
-      "iam:*",
-      "elasticloadbalancing:*",
-      "autoscaling:*",
-      "cloudwatch:*",
-      "cloudfront:*",
-      "route53:*",
-      "ecr:*",
-      "logs:*",
-      "ecs:*",
-      "application-autoscaling:*",
-      "events:*",
-      "elasticache:*",
-      "es:*",
-      "kms:*",
-      "dynamodb:*",
-      "kinesis:*",
-      "firehose:*",
-      "elasticbeanstalk:*",
-      "cloudformation:*",
-      "acm:*",
-      "organizations:*",
-      "sso:*",
-      "identitystore:*",
-    ]
-    resources = ["*"]
-  }
+data "aws_iam_policy" "deploy_allow_specifics" {
+  name = "deploy-allow-specifics"
 }
 
-resource "aws_iam_policy" "deploy_allow_specifics" {
-  name   = "deploy-allow-specifics"
-  policy = data.aws_iam_policy_document.deploy_allow_specifics.json
-}
-
-data "aws_iam_policy_document" "deploy_deny_specifics" {
-  statement {
-    actions = [
-      "iam:*Login*",
-      "iam:*Group*",
-      "aws-portal:*",
-      "budgets:*",
-      "config:*",
-      "directconnect:*",
-      "aws-marketplace:*",
-      "aws-marketplace-management:*",
-      "ec2:*ReservedInstances*"
-    ]
-    effect    = "Deny"
-    resources = ["*"]
-    sid       = "DenySpecifics"
-  }
-}
-
-resource "aws_iam_policy" "deploy_deny_specifics" {
-  name   = "deploy-deny-specifics"
-  policy = data.aws_iam_policy_document.deploy_deny_specifics.json
+data "aws_iam_policy" "deploy_deny_specifics" {
+  name = "deploy-deny-specifics"
 }
 
 resource "aws_iam_role" "this" {
@@ -97,7 +35,7 @@ resource "aws_iam_role" "this" {
 resource "aws_iam_role_policy_attachments_exclusive" "this" {
   role_name = aws_iam_role.this.name
   policy_arns = [
-    aws_iam_policy.deploy_allow_specifics.arn,
-    aws_iam_policy.deploy_deny_specifics.arn,
+    data.aws_iam_policy.deploy_allow_specifics.arn,
+    data.aws_iam_policy.deploy_deny_specifics.arn,
   ]
 }

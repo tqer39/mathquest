7da5319059b4b82e1f46e962072cd083ee7f66f5
tqer39 <tqer39@gmail.com>
2025-09-23 10:09:04 +0900

    cloudflareプロバイダーの明示的なソース指定を追加し、バージョンを設定

---
+++ infra/terraform/modules/cf-app-resources/versions.tf
@@ -0,0 +1,9 @@
+terraform {
+  required_providers {
+    # Terraform が誤って hashicorp/cloudflare を参照しなくなるようにするため、source を明示的に指定
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "4.29.0"
+    }
+  }
+}
